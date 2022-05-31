00986e1b30e20efca7d576cf6159ec8070c79164
pavelmvl <pavelmvl@gmail.com>
2022-05-31 22:48:52 +0300

    lesson08/homework/basket.js: add empty object basket

--- lesson08/homework/basket.js
+++ lesson08/homework/basket.js
@@ -4,3 +4,14 @@ const basketEl = document.querySelector(".basket");
 cartIconWrapEl.addEventListener('click', event => {
   basketEl.classList.toggle("hidden");
 });
+
+/**
+ * В корзине хранится количество каждого товара
+ * Ключ это id продукта, значение это товар в корзине - объект, содержащий
+ * id, название товара, цену, и количество штук, например:
+ * {
+ *    1: {id: 1, name: "product 1", price: 30, count: 2},
+ *    3: {id: 3, name: "product 3", price: 25, count: 1},
+ * }
+ */
+ const basket = {};
